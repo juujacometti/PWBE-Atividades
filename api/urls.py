69dc850e2accee9d7f2ee00ced53b983a9d294d3
juujacometti <julyajacometty@gmail.com>
2025-08-14 16:55:04 -0300

    Aula 04 - 14/08/2025

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
+from .views import visualizacao_autor
 
 urlpatterns = [
     path('autores', AutoresView.as_view()),
+    path('authors', visualizacao_autor)
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
-from .models import Autor
-from .serializers import AutorSerializers
+# OBJETIVO: Expor uma endpoint da API que permite listar todos os autores (GET) e criar um autor novo (POST).
 
+from django.shortcuts import render # Importa função para renderizar templates HTML
+from rest_framework.generics import ListCreateAPIView # Importa a view genérica que já implementa: GET (lista) e POST (criar)
+from .models import Autor # Importa o modelo Autor criado
+from .serializers import AutorSerializers # Importa o serializador que converte o Autor em dados JSON e valida dados de entrada
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+# Cria uam view baseada em classe que permite listar e criar autores via API
 class AutoresView(ListCreateAPIView):
-    queryset = Autor.objects.all()
-    serializer_class = AutorSerializers
+    queryset = Autor.objects.all() # Define a fonte de dados (Todos os registros do modelo Autor)
+    serializer_class = AutorSerializers # Define o serializador usado para serializar (saída) e desserializar/validar (entrada)
+
+@api_view(['GET', 'POST'])
+def visualizacao_autor(request):
+    if request.method == 'GET':
+        queryset = Autor.objects.all()
+        serializer = AutorSerializers(queryset, many = True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = AutorSerializers(data = request.data)
+        if serializer.save():
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.data, status = status.HTTP_400_BAD_REQUEST)
+
+# IMPORTANTE: O serializador é quem traduz objetos Python/Django em formato de dados usados na API (normalmente JSON)
+# Serializar: Transforma os dados do banco de dados (objetos) em JSON para enviar na resposta da API
+# Desserializar: Receber e validar dados pelo usuário em JSON e converter para objetos Python prontos para salvar no banco
+# Decorator: Função que recebe outra função como argumento e estende ou modifica seu comportamento sem alterar diretamente o código da função original
